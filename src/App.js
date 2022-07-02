e3afeb995e0c43d17068f6c6843983e326c344d1
Greg <greg.rodriguez@outlook.com>
2022-07-02 10:58:58 -0600

    made card list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import Cardlist from './components/card-list/card-list.component';
 import './App.css';
 import { Component } from 'react';
 
@@ -10,25 +10,18 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
-    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(() => {
         return {monsters: users}
-      },
-      () => {
-        console.log(this.state);
-      }
-      )
-    );
+      })
+      );
   }
 
   onSearchChange = (event) => {
-    console.log(event.target.value);
     const searchField = event.target.value.toLowerCase();
     this.setState(() => {
       return { searchField };
@@ -36,7 +29,6 @@ class App extends Component {
   }
 
   render() {
-    console.log('render');
 
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
@@ -53,13 +45,7 @@ class App extends Component {
           placeholder='search monsters' 
           onChange={onSearchChange} 
         />
-        {filterdMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <Cardlist monsters={filterdMonsters} />
       </div>
     );
   }
